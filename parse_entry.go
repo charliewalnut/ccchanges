910cadb5ef39282d1a0912372dac5e23c1fc30c3
James Robinson <jamesr@chromium.org>
2011-09-18 16:56:30 -0700

    Add .gitignore, pass first test

--- parse_entry.go
+++ parse_entry.go
@@ -1,5 +1,10 @@
 package ccchanges
 
+import (
+    "exp/regexp" // Using perl character classes \d and \s
+    "strings"
+)
+
 type Person struct {
     name, email string
 }
@@ -9,7 +14,20 @@ type Change struct {
     rollout bool
 }
 
+// Adapted from webkitpy/common/checkout/changelog.py
+var dateLineRegexp = regexp.MustCompile(`^(\d{4}-\d{2}-\d{2})\s+(.+?)\s+<([^<>]+)>$`)
+
+func parseCommitter(line string) Person {
+    submatches := dateLineRegexp.FindStringSubmatch(line)
+    if submatches == nil {
+        return Person{}
+    }
+    return Person{submatches[2], submatches[3]}
+}
+
 func ParseEntry(entry string) Change {
     c := Change{}
+    lines := strings.Split(entry, "\n")
+    c.committer = parseCommitter(lines[0])
     return c
 }
