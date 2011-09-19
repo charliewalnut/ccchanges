e0e8163fe53be017bd5b5e0142efa1582c378911
James Robinson <jamesr@chromium.org>
2011-09-18 17:00:43 -0700

    parses reviewer

--- parse_entry.go
+++ parse_entry.go
@@ -15,19 +15,31 @@ type Change struct {
 }
 
 // Adapted from webkitpy/common/checkout/changelog.py
-var dateLineRegexp = regexp.MustCompile(`^(\d{4}-\d{2}-\d{2})\s+(.+?)\s+<([^<>]+)>$`)
+var dateLineRE = regexp.MustCompile(`^20\d{2}-\d{2}-\d{2}\s+(.+?)\s+<([^<>]+)>$`)
+var reviewerRE = regexp.MustCompile(`Reviewed by (.*?)[\.]`)
 
 func parseCommitter(line string) Person {
-    submatches := dateLineRegexp.FindStringSubmatch(line)
+    submatches := dateLineRE.FindStringSubmatch(line)
     if submatches == nil {
         return Person{}
     }
-    return Person{submatches[2], submatches[3]}
+    return Person{submatches[1], submatches[2]}
+}
+
+func parseReviewer(lines []string) Person {
+    for i := 0; i < len(lines); i++ {
+        submatches := reviewerRE.FindStringSubmatch(lines[i])
+        if submatches != nil {
+            return Person{submatches[1], ""}
+        }
+    }
+    return Person{}
 }
 
 func ParseEntry(entry string) Change {
     c := Change{}
     lines := strings.Split(entry, "\n")
     c.committer = parseCommitter(lines[0])
+    c.reviewer = parseReviewer(lines[1:])
     return c
 }
